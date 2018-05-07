329db699f6121f0de2c981a63a397474df1b8e32
TRIPLE-B <borbae02@gmail.com>
2018-05-07 17:57:23 +0900

    facade main

--- facade/src/com/pattern/facade/Main.java
+++ facade/src/com/pattern/facade/Main.java
@@ -2,4 +2,9 @@ package com.pattern.facade;
 
 public class Main {
 
+  public static void main(String[] args) {
+    HomeTheaterFacade homeTheaterFacade = new HomeTheaterFacade(new Amplifier(), new Tuner(), new DvdPlayer(), new CdPlayer(), new Projector(), new Screen(), new PopcornPopper());
+    homeTheaterFacade.watchMovie("movie");
+    homeTheaterFacade.endMovie();
+  }
 }
